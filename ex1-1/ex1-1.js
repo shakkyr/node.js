729c0b4773497b2dbf8134bca2f6ed6220317341
Shadi Rada <shakkyr@gmail.com>
2021-11-07 13:26:08 +0200

    ex1-1

---
+++ ex1-1/ex1-1.js
@@ -0,0 +1,46 @@
+// Write in your own words the following:
+// What are 3 things node.js has that the client side javascript doesn’t
+// have. And the reasons why.
+// What are 3 things the client side javascript has that node.js doesn’t
+// have. And the reasons why.
+
+
+
+// 1. Javascript is a programming language that is used for writing scripts on the website. 
+
+//     NodeJS is a Javascript runtime environment.
+
+
+// 2.	Javascript can only be run in the browsers.
+
+//     We can run Javascript outside the browser with the help of NodeJS.
+
+
+// 3.	It is basically used on the client-side.
+
+//     It is mostly used on the server-side.
+
+
+
+// 4.	Javascript is capable enough to add HTML and play with the DOM.
+
+//  	Nodejs does not have capability to add HTML tags.
+
+
+// 5.	Javascript can run in any browser engine as like JS core in safari and Spidermonkey in Firefox. 
+
+//     V8 is the Javascript engine inside of node.js that parses and runs Javascript. 
+
+
+// 6.	Javascript is used in frontend development.
+
+//     Nodejs is used in server-side development.
+
+
+// 7.	Some of the javascript frameworks are RamdaJS, TypedJS, etc. 
+
+//     Some of the Nodejs modules are Lodash, express etc. These modules are to be imported from npm. 
+
+
+//  8.	It is the upgraded version of ECMA script that uses Chrome’s V8 engine written in C++. 
+//     Nodejs is written in C, C++ and Javascript.
